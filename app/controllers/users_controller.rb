ef1954851887df31a9faeeb199d6ffacec5a7842
Jack <jackgoh0@gmail.com>
2015-10-20 16:15:16 +0800

    -Init -Added MDL,Devise,Slim,Slick gem -Added mdl header and drawer -Added Drawer,Header, Image slider content

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
+
+  def dashboard
+     @users = User.all
+  end
+
+end

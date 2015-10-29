7adf82b7586c97650a47733f7a3a7d85ab2a4e28
Jack <jackgoh0@gmail.com>
2015-10-29 21:06:56 +0800

    Fixed image size Added header to layout Added read more button link to club page

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,5 +1,5 @@
 class Club < ActiveRecord::Base
-	has_attached_file :logo_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :logo_image, :styles => { :medium => "150x150>", :thumb => "100x100>" }
     validates_attachment_presence :logo_image
     validates_attachment_content_type :logo_image, :content_type => %w(image/jpeg image/jpg image/png)
    
